c245a92057689f1e759018d52399969dcdc48ce4
ihurrahi <andylau1991@gmail.com>
2017-05-23 07:13:14 +0000

    Properly download and wait for MR's

--- background.js
+++ background.js
@@ -1,13 +1,49 @@
-var inProgress = true;
+var inProgress = false;
 var seen = {};
 var childTabs = [];
+var childDownloads = {};
+var mainTab = null;
+var caseIdMapping = {};
 
+// TODO: make a diagram for the flows here because it's all very confusing
 chrome.tabs.onRemoved.addListener(
   function(tabId, removeInfo) {
     for (var i = 0; i < childTabs.length; i++) {
       if (childTabs[i] == tabId) {
         childTabs.splice(i, 1);
+        if (childTabs.length == 0) {
+          chrome.tabs.sendMessage(mainTab, {"message": "mainTab.childrenDoneProcessing"});
+        }
+        break;
       }
+    }
+  }
+);
+
+// Waits for download to finish, then looks up appropriate tab and sends downloadFinished message
+chrome.downloads.onChanged.addListener(
+  function(downloadData) {
+    if (downloadData.id in childDownloads) {
+      tabId = childDownloads[downloadData.id];
+      chrome.tabs.sendMessage(tabId, {"message": "caseLog.downloadFinished"});
+    }
+  }
+);
+
+// Links download to tab id
+chrome.downloads.onCreated.addListener(
+  function(downloadItem) {
+    var link = document.createElement("a");
+    link.href = downloadItem.url;
+    var vars = link.search.substring(1).split("&");
+    var varObj = {};
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      varObj[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+    }
+    if ("myname" in varObj) {
+      var caseId = varObj["myname"].replace("viewdetail-", "");
+      childDownloads[downloadItem.id] = caseIdMapping[caseId];
     }
   }
 );
@@ -16,9 +52,14 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.message == "start") {
       seen = {};
+      mainTab = sender.tab.id;
+      childDownloads = [];
+      caseIdMapping = {};
       inProgress = true;
     } else if (request.message == "inProgress") {
       sendResponse({"inProgress": inProgress});
+    } else if (request.message == "stop") {
+      inProgress = false;
     } else if (request.message == "processedCaseLogs") {
       sendResponse({"seen": seen});
     } else if (request.message == "process") {
@@ -33,6 +74,8 @@ chrome.runtime.onMessage.addListener(
       sendResponse({});
     } else if (request.message == "childTabs") {
       sendResponse(childTabs);
+    } else if (request.message == "download") {
+      caseIdMapping[request.caseId] = sender.tab.id;
     } // TODO: add done message to consolidate results
   }
 );
--- typhon.js
+++ typhon.js
@@ -1,76 +1,110 @@
+var processingInProgress = 0;
+var link = document.createElement("a");
+link.href = window.location.href;
+
 function finishTab(caseId, info) {
   chrome.runtime.sendMessage({"message": "process", "caseLog": caseId}, function(response) {
     window.close();
   });
 }
 
-var link = document.createElement("a");
-link.href = window.location.href;
-if (link.pathname == "/past/data/search.asp") {
-  alert("Starting download of all MR's into PDF's");
-  chrome.runtime.sendMessage({"message": "inProgress"}, function(response) {
-    if (response.inProgress == true) {
-      var timer = setInterval(function() {
-        chrome.runtime.sendMessage({"message": "childTabs"}, function(tabs) {
-          if (tabs.length == 0) {
-            chrome.runtime.sendMessage({"message": "processedCaseLogs"}, function(response) {
-              processing = 0;
-              var tables = document.getElementsByTagName('table');
-              var dataTable = tables[3];  // Magic
-              var links = dataTable.getElementsByTagName('a');
-
-              // Filter to only the links to case logs
-              for (var i = 0; i < links.length; i++) {
-                var link = links[i];
-                if (link.pathname == "/past/data/viewdetail.asp" && !(link.text in response.seen)) {
-                  processing += 1;
-                  chrome.runtime.sendMessage({"message": "newtab", "link": link.href});
-                }
-                if (processing > 3) {
-                  break;
-                }
-              }
-              if (processing == 0) {
-                clearInterval(timer);
-                alert("All done!");
-              }
-            });
+function processCaseLogs() {
+  chrome.runtime.sendMessage({"message": "childTabs"}, function(tabs) {
+    if (tabs.length == 0) {
+      chrome.runtime.sendMessage({"message": "processedCaseLogs"}, function(response) {
+        processing = 0;
+        var tables = document.getElementsByTagName('table');
+        var dataTable = tables[3];  // Magic
+        var links = dataTable.getElementsByTagName('a');
+  
+        // Filter to only the links to case logs
+        for (var i = 0; i < links.length; i++) {
+          var link = links[i];
+          if (link.pathname == "/past/data/viewdetail.asp" && !(link.text in response.seen)) {
+            processing += 1;
+            chrome.runtime.sendMessage({"message": "newtab", "link": link.href});
           }
-        });
-      }, 1001);
+          if (processing > 3) {
+            break;
+          }
+        }
+        if (processing == 0) {
+          chrome.runtime.sendMessage({"message": "stop"}, function(response) {
+            alert("All done!");
+          });
+        }
+      });
     }
   });
-} else if (link.pathname == "/past/data/viewdetail.asp") {
+}
+
+function getCaseId() {
   var tables = document.getElementsByTagName("table");
   var caseTable = tables[2];  // Magic
-  var caseId = caseTable.getElementsByTagName("td")[0].innerText.trim().split("Case ID #: ")[1];
+  return caseTable.getElementsByTagName("td")[0].innerText.trim().split("Case ID #: ")[1];
+}
 
-  var baseInfoTable = tables[3];
-  var codesTable = tables[6];
-  var otherInfoTable = tables[8];
-  var notesTable = tables[9];
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message == "caseLog.downloadFinished") {
+      processingInProgress -= 1;
+      if (processingInProgress == 0) {
+        var caseId = getCaseId();
+        finishTab(caseId, {});
+      }
+    } else if (request.message == "mainTab.childrenDoneProcessing") {
+      processCaseLogs();
+    }
+  }
+);
 
-  var baseInfo = baseInfoTable.getElementsByTagName("tr");
-  var exportButtons = baseInfo[0];
-  var exportPdf = exportButtons.getElementsByTagName("a")[1];
-
-  processing = 0;
-  if (true) {  // TODO: replace with flag
-    otherInfo = otherInfoTable.getElementsByTagName("tr");
-    for (var i = 0; i < otherInfo.length; i++) {
-      if (otherInfo[i].innerText.includes("Minimum Requirement Encounter")) {
-        if (otherInfo[i].innerText.split(":")[1].trim() == "Yes") {
-          processing += 1;
-          exportPdf.click();
-          // TODO: will hang - need to close the tab after the download finishes
+chrome.runtime.sendMessage({"message": "inProgress"}, function(response) {
+  if (link.pathname == "/past/data/search.asp") {
+    if (response.inProgress == true) {
+      processCaseLogs();
+    } else {
+      result = confirm("Do you want to download of all MR's into PDF's? Make sure you have the correct date filter!");
+      if (result) {
+        chrome.runtime.sendMessage({"message": "start"}, function(response) {
+          processCaseLogs();
+        });
+      }
+    }
+  } else if (link.pathname == "/past/data/viewdetail.asp") {
+    if (response.inProgress == true) {
+      var tables = document.getElementsByTagName("table");
+      var caseTable = tables[2];  // Magic
+      var caseId = caseTable.getElementsByTagName("td")[0].innerText.trim().split("Case ID #: ")[1];
+  
+      var baseInfoTable = tables[3];
+      var codesTable = tables[6];
+      var otherInfoTable = tables[8];
+      var notesTable = tables[9];
+  
+      var baseInfo = baseInfoTable.getElementsByTagName("tr");
+      var exportButtons = baseInfo[0];
+      var exportPdf = exportButtons.getElementsByTagName("a")[1];
+  
+      if (true) {  // TODO: replace with flag
+        otherInfo = otherInfoTable.getElementsByTagName("tr");
+        for (var i = 0; i < otherInfo.length; i++) {
+          if (otherInfo[i].innerText.includes("Minimum Requirement Encounter")) {
+            if (otherInfo[i].innerText.split(":")[1].trim() == "Yes") {
+              processingInProgress += 1;
+              chrome.runtime.sendMessage({"message": "download", "caseId": caseId});
+              exportPdf.click();
+            }
+          }
         }
+      }
+  
+      processingInProgress += 1;
+      // TODO: verify constraints here
+      processingInProgress -= 1;
+  
+      if (processingInProgress == 0) {
+        finishTab(caseId, {});
       }
     }
   }
-
-  // TODO: verify constraints here
-
-  if (processing == 0) {
-    finishTab(caseId, {});
-  }
-}
+});
